cce95333435e6ad9bfee60beda418da994a21ff5
Bruno Pontes <brunospontes@hotmail.com>
2020-03-01 22:56:12 -0300

    Disable auctions in DB; Save or update current auctions

--- database/service.py
+++ database/service.py
@@ -3,9 +3,26 @@
 from database.models import Auction
 
 def updateAuctions(source, currentAuctions):
-    # Get all active auctions from source
-    for x in Auction.objects(title__nin=['50% DO IMÓVEL NA QN 513 - SAMAMBAIA/DF']):
-        print(f'->> {x.title}')
+    # Disable active auctions that is not in the current list
+    currentTitles = [auction.title for auction in currentAuctions]
+    Auction.objects(source=source, disabled=False, title__nin=currentTitles).update(
+        set__disabled=True)
 
-    # for currentAuction in currentAuctions:
-    #     currentAuction.save()
+    # Create or update auctions
+    for currentAuction in currentAuctions:
+        Auction.objects(title=currentAuction.title).update_one(
+            set__status=currentAuction.status,
+            set__url=currentAuction.url,
+            set__source=currentAuction.source,
+            set__title=currentAuction.title,
+            set__currentBid=currentAuction.currentBid,
+            set__startingBid=currentAuction.startingBid,
+            set__description=currentAuction.description,
+            set__evaluation=currentAuction.evaluation,
+            set__auction1Opening=currentAuction.auction1Opening,
+            set__auction1Closure=currentAuction.auction1Closure,
+            set__auction1Bid=currentAuction.auction1Bid,
+            set__auction2Opening=currentAuction.auction2Opening,
+            set__auction2Closure=currentAuction.auction2Closure,
+            set__auction2Bid=currentAuction.auction2Bid,
+            upsert=True)
