3c22ec210f80974ebf2932b0be71d6dcb4b8be23
Gustavo Barros Rocha <gus.rochab@gmail.com>
2020-03-19 15:17:09 -0300

    HomePage class deleted and Page updated to make requests

--- page.py
+++ page.py
@@ -1,89 +1,56 @@
-import json
+import requests
+from page_parcer import *
+from urllib3.exceptions import NewConnectionError, LocationParseError
+from requests.exceptions import ConnectionError, InvalidURL
 
 class Page:
-    def __init__(self, link):
-        self.link = link
-        self.text = ""
+    def __init__(self, link, parcer_name='first_parcer'):
+        self.html_status = None
+        self.label = None
+        self.depth = None
         self.links_list = []
-        self.html_ok = ''
-        self.html_status = ''
+        self.text = ''
+        self.link = link
+        self.broken = False
+        headers = {'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"}
+        try:
+            self.html = requests.get(self.link, headers=headers)
+        except ConnectionError as e:
+            self.broken = True
+            self.text = e
+        except NewConnectionError as e:
+            self.broken = True
+            self.text = e
+        except LocationParseError as e:
+            self.broken = True
+            self.text = e
+        except InvalidURL as e:
+            self.broken = True
+            self.text = e
+        if not self.broken:
+            self.parcer = PageParcer(self.html.text, parcer_name)
+
+
+    def get_page_text(self):
+        if not self.broken:
+            try:
+                self.text = self.parcer.text()
+            except UnicodeEncodeError:
+                self.text = f'UnicodeEncodeError. Encode used: {self.encoding}. ' \
+                                f'Apparent encode: {html.apparent_encoding}'
+            # Get the status of the Response object
+            try:
+                self.html_status = self.html.status_code
+            except UnicodeEncodeError:
+                self.html_status = f'UnicodeEncodeError. Encode used: {self.encoding}. ' \
+                                       f'Apparent encode: {html.apparent_encoding}'
+
+    def get_page_links(self, homepage_link):
+        if not self.broken:
+            self.links_list = self.parcer.links(homepage_link)
+
 
     def __repr__(self):
         return self.link
-
-    def page_printer(self):
-        if not page.link:
-            return
-
-
-
-class HomePage(Page):
-    def __init__(self, link):
-        Page.__init__(self, link)
-        self.pages_list = []
-        self.all_links = []
-        self.broken_links = {}
-
-    def __repr__(self):
-        return self.link
-
-    def to_json(self, file_name):
-        """
-        :param file_name: json file name
-        :return: a json file with pages object attributes
-        """
-        pages = {}
-
-        if file_name[-5:] != '.json':
-            file_name = f'{file_name}.json'
-
-        file_path = f'crawled pages/{file_name}'
-
-        for page in self.pages_list:
-            pages[page.link] = {
-                'status': page.html_status,
-                'text': str(page.text)
-            }
-
-        #pages['broken links'] = self.broken_links
-
-        #pages_json = json.dumps(pages, ensure_ascii=False).encode('utf-8')
-        pages_json = json.dumps(pages)
-
-        with open(file_path, 'w') as f:
-            f.write(pages_json.decode())
-
-    def to_txt(self, file_name):
-        """
-        :param file_name: txt file name
-        :return: txt file with pages object attributes
-        """
-        if file_name[-4:] != '.txt':
-            file_name = f'{file_name}.txt'
-
-        file_path = f'crawled pages/{file_name}'
-
-        with open(file_path, 'w') as f:
-            f.write('*************** HOME PAGE ***************\n\n')
-            f.write(f'{self.link}\n\n')
-            f.write(f'-All links:\n\n')
-            f.write(f'Number of links: {len(self.all_links)}\n\n')
-            f.write(f'{self.all_links}\n\n')
-            f.write('-All pages\n\n')
-            f.write(f'Number of pages: {len(self.pages_list)}\n\n')
-            f.write('-Broken links\n\n')
-            f.write(f'Number of broken links: {len(self.broken_links)}\n\n')
-            f.write(f'{self.broken_links}\n\n')
-            f.write('-Pages status\n\n')
-            for page in self.pages_list:
-                f.write(f'{page.link} - {page.html_status}\n')
-            f.write('\n\n')
-            f.write('-Home page text\n\n')
-            f.write(f'{self.text}\n\n\n\n')
-            for page in self.pages_list:
-                f.write('-------------- NEW PAGE --------------\n\n')
-                f.write(f'{page.link}\n\n')
-                f.write('Page text\n\n')
-                f.write(f'{page.text}\n\n\n\n')
 
 
